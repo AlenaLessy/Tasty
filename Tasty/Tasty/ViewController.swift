efd8a05ac743282cd1611e93caef377ef7b8f694
Алена Карпова <alenapancenko00@gmail.com>
2023-02-16 11:36:24 +0300

    init

---
+++ Tasty/Tasty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasty
+//
+//  Created by Алена Панченко on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
